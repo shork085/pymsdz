b2b99d91e09510116c67a043f4c64146768324a6
shork085 <115993592+shork085@users.noreply.github.com>
2023-02-16 09:47:18 +0800

    Update name.js

--- JavaScript/name.js
+++ JavaScript/name.js
@@ -13,7 +13,10 @@ window.onload = function () {
     backm.paused = ispaused;
 
     document.addEventListener('click', function() {
-        document.getElementById('bgm').play();
+        if(backm.paused){
+        }else{
+            backm.play();
+        }
     });
 }
 
