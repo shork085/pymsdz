977b1c5630b090815babc597ddd709577b11b5fa
shork085 <115993592+shork085@users.noreply.github.com>
2023-02-16 10:00:21 +0800

    Update page2.js

--- JavaScript/page2.js
+++ JavaScript/page2.js
@@ -90,7 +90,9 @@ window.onload = function () {
 
     document.addEventListener('mouseup', function() {
         if(backm.paused){
+            horn.innerHTML = "";
         }else{
+            horn.innerHTML = "";
             backm.play();
         }
     });
